75ed2a0c05fa0d46f88979f326924ad3fd1cb3c7
Jean-Paul Calderone <exarkun@divmod.com>
2007-11-05 19:55:03 -0500

    some refactoring to only pass --unclean-warnings to some of the builders

--- master/twisted_factories.py
+++ master/twisted_factories.py
@@ -11,7 +11,9 @@ from twisted_steps import HLint, ProcessDocs, BuildDebs, \
     Trial, RemovePYCs, CheckDocumentation
 
 
-TRIAL_FLAGS = ["--reporter=bwverbose", "--unclean-warnings"]
+TRIAL_FLAGS = ["--reporter=bwverbose"]
+WARNING_FLAGS = ["--unclean-warnings"]
+FORCEGC_FLAGS = ["--force-gc"]
 
 class TwistedBuild(Build):
     workdir = "Twisted" # twisted's bin/trial expects to live in here
@@ -37,16 +39,27 @@ class TwistedBaseFactory(BuildFactory):
     # this to add the local tree to PYTHONPATH during tests
     workdir = "Twisted"
 
-    def __init__(self, source):
+    forceGarbageCollection = False
+
+    def __init__(self, source, uncleanWarnings):
         BuildFactory.__init__(self, [source])
+        self.uncleanWarnings = uncleanWarnings
+
+    def addTrialStep(self, **kw):
+        trialMode = TRIAL_FLAGS
+        if self.uncleanWarnings:
+            trialMode = trialMode + WARNING_FLAGS
+        if self.forceGarbageCollection:
+            trialMode = trialMode + FORCEGC_FLAGS
+        self.addStep(TwistedTrial, trialMode=trialMode, **kw)
 
 
 class QuickTwistedBuildFactory(TwistedBaseFactory):
     treeStableTimer = 30
     useProgress = 0
 
-    def __init__(self, source, python="python"):
-        TwistedBaseFactory.__init__(self, source)
+    def __init__(self, source, python="python", uncleanWarnings=True):
+        TwistedBaseFactory.__init__(self, source, uncleanWarnings)
         if type(python) is str:
             python = [python]
         self.addStep(HLint, python=python[0])
@@ -54,7 +67,7 @@ class QuickTwistedBuildFactory(TwistedBaseFactory):
         for p in python:
             cmd = [p, "setup.py", "build_ext", "-i"]
             self.addStep(shell.Compile, command=cmd, flunkOnFailure=True)
-            self.addStep(TwistedTrial, python=p, testChanges=True)
+            self.addTrialStep(python=p, testChanges=True)
 
 
 
@@ -62,7 +75,7 @@ class TwistedDocumentationBuildFactory(TwistedBaseFactory):
     treeStableTimer = 5 * 60
 
     def __init__(self, source, python="python"):
-        TwistedBaseFactory.__init__(self, source)
+        TwistedBaseFactory.__init__(self, source, False)
         self.addStep(CheckDocumentation)
         self.addStep(ProcessDocs)
 
@@ -73,8 +86,9 @@ class FullTwistedBuildFactory(TwistedBaseFactory):
 
     def __init__(self, source, python="python",
                  runTestsRandomly=False,
-                 compileOpts=[], compileOpts2=[]):
-        TwistedBaseFactory.__init__(self, source)
+                 compileOpts=[], compileOpts2=[],
+                 uncleanWarnings=True):
+        TwistedBaseFactory.__init__(self, source, uncleanWarnings)
 
         if type(python) == str:
             python = [python]
@@ -85,7 +99,7 @@ class FullTwistedBuildFactory(TwistedBaseFactory):
 
         self.addStep(shell.Compile, command=cmd, flunkOnFailure=True)
         self.addStep(RemovePYCs)
-        self.addStep(TwistedTrial, python=python, randomly=runTestsRandomly)
+        self.addTrialStep(python=python, randomly=runTestsRandomly)
 
 
 class TwistedDebsBuildFactory(TwistedBaseFactory):
@@ -102,8 +116,8 @@ class TwistedReactorsBuildFactory(TwistedBaseFactory):
 
     def __init__(self, source,
                  python="python", compileOpts=[], compileOpts2=[],
-                 reactors=None):
-        TwistedBaseFactory.__init__(self, source)
+                 reactors=None, uncleanWarnings=True):
+        TwistedBaseFactory.__init__(self, source, uncleanWarnings)
 
         if type(python) == str:
             python = [python]
@@ -132,9 +146,10 @@ class TwistedReactorsBuildFactory(TwistedBaseFactory):
             #    flunkOnFailure = 0
             #    warnOnFailure = 1
             self.addStep(RemovePYCs) # TODO: why?
-            self.addStep(TwistedTrial, name=reactor, python=python,
-                         reactor=reactor, flunkOnFailure=flunkOnFailure,
-                         warnOnFailure=warnOnFailure)
+            self.addTrialStep(
+                name=reactor, python=python,
+                reactor=reactor, flunkOnFailure=flunkOnFailure,
+                warnOnFailure=warnOnFailure)
 
 
 class Win32RemovePYCs(ShellCommand):
@@ -144,17 +159,16 @@ class Win32RemovePYCs(ShellCommand):
     descriptionDone = ["remove", ".pycs"]
 
 
-class GoodTrial(TwistedTrial):
-    trialMode = TRIAL_FLAGS + ["--force-gc"]
-
-
 class GoodTwistedBuildFactory(TwistedBaseFactory):
     treeStableTimer = 5 * 60
 
+    forceGarbageCollection = True
+
     def __init__(self, source, python="python",
                  processDocs=False, runTestsRandomly=False,
-                 compileOpts=[], compileOpts2=[]):
-        TwistedBaseFactory.__init__(self, source)
+                 compileOpts=[], compileOpts2=[],
+                 uncleanWarnings=True):
+        TwistedBaseFactory.__init__(self, source, uncleanWarnings)
         if processDocs:
             self.addStep(ProcessDocs)
 
@@ -167,7 +181,7 @@ class GoodTwistedBuildFactory(TwistedBaseFactory):
 
         self.addStep(shell.Compile, command=cmd, flunkOnFailure=True)
         self.addStep(RemovePYCs)
-        self.addStep(GoodTrial, python=python, randomly=runTestsRandomly)
+        self.addTrialStep(python=python, randomly=runTestsRandomly)
 
 
 class TwistedReactorsBuildFactory(TwistedBaseFactory):
@@ -175,8 +189,8 @@ class TwistedReactorsBuildFactory(TwistedBaseFactory):
 
     def __init__(self, source, RemovePYCs=RemovePYCs,
                  python="python", compileOpts=[], compileOpts2=[],
-                 reactors=["select"]):
-        TwistedBaseFactory.__init__(self, source)
+                 reactors=["select"], uncleanWarnings=True):
+        TwistedBaseFactory.__init__(self, source, uncleanWarnings)
 
         if type(python) == str:
             python = [python]
@@ -189,6 +203,7 @@ class TwistedReactorsBuildFactory(TwistedBaseFactory):
 
         for reactor in reactors:
             self.addStep(RemovePYCs)
-            self.addStep(TwistedTrial, name=reactor, python=python,
-                         reactor=reactor, flunkOnFailure=True,
-                         warnOnFailure=False)
+            self.addTrialStep(
+                name=reactor, python=python,
+                reactor=reactor, flunkOnFailure=True,
+                warnOnFailure=False)

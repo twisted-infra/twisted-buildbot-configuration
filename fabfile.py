bdb6aacf70382d32c30c5c91503a49a36df4d4be
Tom Prince <tom.prince@ualberta.net>
2013-05-09 11:36:53 -0600

    Use correct user for testInit.

--- fabfile.py
+++ fabfile.py
@@ -38,7 +38,8 @@ class Buildbot(service.Service):
         if env.get('environment') == 'production':
            abort("Don't use testInit in production.")
 
-        with cd(os.path.join(self.configDir, 'master')):
+        with (settings(user=self.serviceUser),
+             cd(os.path.join(self.configDir, 'master'))):
             if force or not files.exists('private.py'):
                 puts('Using sample private.py')
                 run('cp private.py.sample private.py')

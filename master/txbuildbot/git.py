e041a1433593ee85727eb5ab7a08f8ae9d748e83
Christopher Armstrong <radix@twistedmatrix.com>
2013-06-16 18:12:09 -0500

    move TwistedGit into txbuildbot.git, and subclass from the newer Git step.

---
+++ master/txbuildbot/git.py
@@ -0,0 +1,21 @@
+from buildbot.steps.source.git import Git
+
+
+class TwistedGit(Git):
+    """
+    This should be *very* temporary; it's to support people trying to force
+    builds against /branches/foo and letting it work on the git builders.
+    People should start switching to just "foo", but while we have both
+    SVN-based and git-based builders going it'll be impossible to force
+    builds on all bots at the same time with the same command.
+    """
+
+    def startVC(self, branch, revision, patch):
+        """
+        If a branch name starts with /branches/, cut it off before referring
+        to it in git commands.
+        """
+        cutoff = "/branches/"
+        if branch.startswith(cutoff):
+            branch = branch[len(cutoff):]
+        return Git.startVC(self, branch, revision, patch)

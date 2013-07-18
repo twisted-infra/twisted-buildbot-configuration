6cd247d20aebfaef09efd3f353d4015e999c16ab
Tom Prince <tom.prince@ualberta.net>
2013-07-18 17:31:21 -0600

    Make git get proper version for lint steps.

--- master/txbuildbot/git.py
+++ master/txbuildbot/git.py
@@ -1,6 +1,9 @@
+from twisted.python import log
+
+from buildbot.process import buildstep
 from buildbot.steps.source.git import Git
-from buildbot.steps.shell import ShellCommand
-from buildbot.status.results import SKIPPED
+from buildbot.steps.source import Source
+from buildbot.status.results import SUCCESS
 
 
 class TwistedGit(Git):
@@ -29,7 +32,7 @@ class TwistedGit(Git):
 
 
 
-class MergeForward(ShellCommand):
+class MergeForward(Source):
     """
     Merge with trunk.
     """
@@ -39,10 +42,7 @@ class MergeForward(ShellCommand):
     haltOnFailure = True
 
 
-    def __init__(self, repourl, **kwargs):
-        kwargs['command'] = ['git', 'pull',
-                             '--no-ff', '--no-stat',
-                             repourl, 'trunk']
+    def __init__(self, repourl, branch, **kwargs):
         kwargs['env'] = {
                 'GIT_MERGE_AUTOEDIT': 'no',
                 'GIT_AUTHOR_EMAIL': 'buildbot@twistedmatrix.com',
@@ -50,26 +50,67 @@ class MergeForward(ShellCommand):
                 'GIT_COMMITTER_EMAIL': 'buildbot@twistedmatrix.com',
                 'GIT_COMMITTER_NAME': 'Twisted Buildbot',
                 }
-        ShellCommand.__init__(self, **kwargs)
-        self.addFactoryArguments(repourl=repourl)
+        Source.__init__(self, **kwargs)
+        self.addFactoryArguments(repourl=repourl, branch=branch)
 
 
-    def doStepIf(self, _):
-        """
-        Don't merge if building trunk or a release.
-        """
-        stamp = self.build.getSourceStamp(None)
-        branch = stamp.branch
+    def startVC(self, branch, revision, patch):
+        branch = self.mungeBranch(branch)
 
+        self.stdio_log = self.addLog('stdio')
+
+        d = self._fetch()
+        if self.shouldMerge(branch):
+            d.addCallback(lambda _: self._merge())
+        if self.isTrunk(branch):
+            d.addCallback(lambda _: self._getPreviousVersion())
+        else:
+            d.addCallback(lambda _: self._getMergeBase())
+        d.addCallback(self._setLintVersion)
+
+        d.addCallback(lambda _: SUCCESS)
+        d.addCallbacks(self.finished, self.checkDisconnect)
+        d.addErrback(self.failed)
+
+    def _fetch(self):
+        return self._dovccmd(['fetch', self.repourl, 'trunk'])
+
+    def _merge(self):
+        return self._dovccmd(['merge',
+                              '--no-ff', '--no-stat',
+                              'FETCH_HEAD'])
+
+    def _getPreviousVersion(self):
+        return self._dovccmd(['rev-parse', 'FETCH_HEAD'],
+                              collectStdout=True)
+
+    def _getMergeBase(self):
+        return self._dovccmd(['merge-base', 'HEAD', 'FETCH_HEAD'],
+                              collectStdout=True)
+
+    def _setLintVersion(self, version):
+        self.setProperty("lint_version", version, "merge-forward")
+
+    def isTrunk(self, branch):
+        return branch in ('', 'trunk')
+
+
+    def mungeBranch(self, branch):
         if branch is None:
-            return False
+            branch = ''
 
         for cutoff in ['/branches/', 'branches/', '/']:
             if branch.startswith(cutoff):
                 branch = branch[len(cutoff):]
                 break
+        return branch
 
-        if branch in ('', 'trunk'):
+
+    def shouldMerge(self, branch):
+        """
+        Don't merge if building trunk or a release.
+        """
+        if self.isTrunk(branch):
             return False
 
         if branch.startswith('releases/'):
@@ -78,5 +119,21 @@ class MergeForward(ShellCommand):
         return True
 
 
-    def hideStepIf(self, result, _):
-        return result == SKIPPED
+    def _dovccmd(self, command, abandonOnFailure=True, collectStdout=False, extra_args={}, env = None):
+        cmd = buildstep.RemoteShellCommand(self.workdir, ['git'] + command,
+                                           env=self.env,
+                                           logEnviron=self.logEnviron,
+                                           collectStdout=collectStdout,
+                                           **extra_args)
+        cmd.useLog(self.stdio_log, False)
+        d = self.runCommand(cmd)
+        def evaluateCommand(cmd):
+            if abandonOnFailure and cmd.rc != 0:
+                log.msg("Source step failed while running command %s" % cmd)
+                raise buildstep.BuildStepFailed()
+            if collectStdout:
+                return cmd.stdout
+            else:
+                return cmd.rc
+        d.addCallback(lambda _: evaluateCommand(cmd))
+        return d
